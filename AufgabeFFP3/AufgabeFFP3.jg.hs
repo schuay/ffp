2e9ffc630efea3421f182ddf7d67c0679d72288a
Jakob Gruber <jakob.gruber@gmail.com>
2012-04-19 21:08:22 +0200

    Implementation of assignment #3 (jg)

--- AufgabeFFP3/AufgabeFFP3.jg.hs
+++ AufgabeFFP3/AufgabeFFP3.jg.hs
@@ -1,5 +1,7 @@
 module AufgabeFFP3 where
 
+import Data.List (delete)
+
 type Weight = Int
 type Value = Int
 type Item = (Weight, Value)
@@ -12,29 +14,53 @@ type MaxWeight = Weight
 {-- Generates all possible combinations of items. --}
 
 generator :: Items -> Loads
-generator = undefined
+generator = select
+
+{-- Returns all possible selections of elements in a. --}
+
+select :: Eq a => [a] -> [[a]]
+select (x:xs) = [[x]] ++ ys ++ [ x : y | y <- ys ]
+    where ys = select (delete x xs)
+select _ = []
 
 {-- Calculates total weight and value for each load. --}
 
 transformer :: Loads -> [LoadWghtVal]
-transformer = undefined
+transformer = map metrics
+
+{-- Calculates weight and value for a single load. --}
+
+metrics :: Load -> LoadWghtVal
+metrics = foldr metric ([], 0, 0)
+
+{-- Takes a LoadWghtVal, and updates it with the current item. --}
+
+metric :: Item -> LoadWghtVal -> LoadWghtVal
+metric l@(w, v) (l', w', v') = (l:l', w + w', v + v')
 
 {-- Filters input to combinations which don't exceed the maximum
  - weight. --}
 
 filter :: [LoadWghtVal] -> MaxWeight -> [LoadWghtVal]
-filter = undefined
+filter xs w = Prelude.filter (\(_, w', _) -> w' <= w) xs
 
 {-- Selects combination(s) with the highest value. --}
 
 selector1 :: [LoadWghtVal] -> [LoadWghtVal]
-selector1 = undefined
+selector1 xs = Prelude.filter (\(_, _, v) -> v == maxval xs) xs
+
+maxval :: [LoadWghtVal] -> Int
+maxval xs = maximum $ [ v | (_, _, v) <- xs ]
+
+minwgt :: [LoadWghtVal] -> Int
+minwgt xs = minimum $ [ w | (_, w, _) <- xs ]
 
 {-- Selects combination(s) which achieve the highest value with
  - minimal weight. --}
 
 selector2 :: [LoadWghtVal] -> [LoadWghtVal]
-selector2 = undefined
+selector2 xs = Prelude.filter (\(_, w, v) -> w == minwgt xs') xs'
+    where xs' = selector1 xs
 
 {-- Binomial coefficients, naive implementation. --}
 
@@ -46,9 +72,36 @@ binom (n,k)
 {-- Binomial coefficients, stream implementation. --}
 
 binomS :: (Integer,Integer) -> Integer
-binomS = undefined
+binomS (n, k)
+    | n < 0 || k < 0 || n < k = 0
+    | otherwise = (pascal !! n') !! k'
+    where n' = fromIntegral n
+          k' = fromIntegral k
+
+{-- Stream of pascal triangle rows. --}
+
+pascal :: [[Integer]]
+pascal = [1] : pascal' [1]
+
+pascal' :: [Integer] -> [[Integer]]
+pascal' xs = next : pascal' next
+    where next = 1 : zipWith (+) xs (tail xs) ++ [1]
 
 {-- Binomial coefficients, memoization implementation. --}
 
 binomM :: (Integer,Integer) -> Integer
-binomM = undefined
+binomM (n, k)
+    | n < 0 || k < 0 || n < k = 0
+    | otherwise = (pascaltable !! n') !! k'
+    where n' = fromIntegral n
+          k' = fromIntegral k
+
+{-- Memo table of pascal triangle rows. --}
+
+pascaltable :: [[Integer]]
+pascaltable = [ pascalmemo n | n <- [0 ..] ]
+
+pascalmemo :: Int -> [Integer]
+pascalmemo 0 = [1]
+pascalmemo n = 1 : zipWith (+) prev (tail prev) ++ [1]
+    where prev = pascaltable !! (n - 1)
