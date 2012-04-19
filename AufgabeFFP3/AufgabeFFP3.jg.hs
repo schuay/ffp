b0efff91dad8c1aa1e59df57b01a10788d4ae0b5
Jakob Gruber <jakob.gruber@gmail.com>
2012-04-19 20:33:33 +0200

    Comment all public functions

--- AufgabeFFP3/AufgabeFFP3.jg.hs
+++ AufgabeFFP3/AufgabeFFP3.jg.hs
@@ -9,29 +9,46 @@ type Loads = [Load]
 type LoadWghtVal = (Load, Weight, Value)
 type MaxWeight = Weight
 
+{-- Generates all possible combinations of items. --}
+
 generator :: Items -> Loads
 generator = undefined
+
+{-- Calculates total weight and value for each load. --}
 
 transformer :: Loads -> [LoadWghtVal]
 transformer = undefined
 
+{-- Filters input to combinations which don't exceed the maximum
+ - weight. --}
+
 filter :: [LoadWghtVal] -> MaxWeight -> [LoadWghtVal]
 filter = undefined
+
+{-- Selects combination(s) with the highest value. --}
 
 selector1 :: [LoadWghtVal] -> [LoadWghtVal]
 selector1 = undefined
 
+{-- Selects combination(s) which achieve the highest value with
+ - minimal weight. --}
+
 selector2 :: [LoadWghtVal] -> [LoadWghtVal]
 selector2 = undefined
+
+{-- Binomial coefficients, naive implementation. --}
 
 binom :: (Integer, Integer) -> Integer
 binom (n,k)
     | k == 0 || n == k = 1
     | otherwise = binom (n - 1, k - 1) + binom (n - 1, k)
 
+{-- Binomial coefficients, stream implementation. --}
 
 binomS :: (Integer,Integer) -> Integer
 binomS = undefined
+
+{-- Binomial coefficients, memoization implementation. --}
 
 binomM :: (Integer,Integer) -> Integer
 binomM = undefined
