1b7d47de8f9a3869318e0099dd22adc9d39e1961
Jakob Gruber <jakob.gruber@gmail.com>
2012-05-21 20:14:16 +0200

    Don't run dupe tests on fn's that can't handle them

--- AufgabeFFP8/Test.hs
+++ AufgabeFFP8/Test.hs
@@ -3,27 +3,33 @@ module Main where
 import AufgabeFFP8
 import Test.HUnit
 
-tests = [ ("Empty list", 0, [])
+uniqTests =
+        [ ("Empty list", 0, [])
         , ("Unsorted list, free at front", 0, [2,3,1])
         , ("Unsorted list, free in middle", 2, [1,0,3])
         , ("Unsorted list, free in middle", 5, [1,0,3,10,2,11,20,103,4,6,12])
         , ("Unsorted list, free at end", 3, [2,1,0])
-        , ("Unsorted list with duplicates, free at front", 0, [2,2,3,1,3])
+        ]
+dupTests =
+        [ ("Unsorted list with duplicates, free at front", 0, [2,2,3,1,3])
         , ("Unsorted list with duplicates, free in middle", 2, [0,1,0,3,1])
         , ("Unsorted list with duplicates, free in middle", 2, [0,1,1,0,4,7,12,14,342,1,1,1,3,4,1])
         , ("Unsorted list with duplicates, free at end", 3, [2,1,0,0,2])
         ]
 
-testFns f = map (TestCase . assertEqual' f) tests
+testFns tests f = map (TestCase . assertEqual' f) tests
     where assertEqual' f (desc, expected, args) = assertEqual desc expected (f args)
 
-main = runTestTT $ TestList [ TestList (testFns minfree_bv)
-                            , TestList (testFns minfree_chl)
-                            , TestList (testFns minfree_col)
-                            , TestList (testFns minfree_b)
-                            , TestList (testFns minfree_r)
-                            , TestList (testFns minfree_o)
-                            , TestList (testFns minfree_bhof)
-                            , TestList (testFns minfree_rhof)
-                            , TestList (testFns minfree_ohof)
+main = runTestTT $ TestList [ TestList (testFns uniqTests minfree_bv)
+                            , TestList (testFns uniqTests minfree_chl)
+                            , TestList (testFns uniqTests minfree_col)
+                            , TestList (testFns dupTests minfree_bv)
+                            , TestList (testFns dupTests minfree_chl)
+                            , TestList (testFns dupTests minfree_col)
+                            , TestList (testFns uniqTests minfree_b)
+                            , TestList (testFns uniqTests minfree_r)
+                            , TestList (testFns uniqTests minfree_o)
+                            , TestList (testFns uniqTests minfree_bhof)
+                            , TestList (testFns uniqTests minfree_rhof)
+                            , TestList (testFns uniqTests minfree_ohof)
                             ]
