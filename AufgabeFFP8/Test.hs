8643e65edfc7161c6c58257996f44a317e33e014
c553db14 <c553db14@example.com>
2012-05-22 12:31:48 +0200

    Add additional test case

--- AufgabeFFP8/Test.hs
+++ AufgabeFFP8/Test.hs
@@ -9,6 +9,7 @@ uniqTests =
         , ("Unsorted list, free at front", 0, [2,3,1])
         , ("Unsorted list, free in middle", 2, [1,0,3])
         , ("Unsorted list, free in middle", 5, [1,0,3,10,2,11,20,103,4,6,12])
+        , ("Unsorted list, free in middle", 1, [2,3,0,4])
         , ("Unsorted list, free at end", 3, [2,1,0])
         , ("Unsorted list, free at end", 1, [0])
         ]
